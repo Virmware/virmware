9b77edb07e2c07a03c3eec3a4361e5ddf4a412dc
virmware <stephen@virmware.com>
2020-12-01 10:10:13 +0800

    Initial commit

---
+++ cases.js
@@ -0,0 +1,38 @@
+
+        new Vue({
+            el: '#app',
+            data: {
+                cases: [
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333306115862319104', 
+ title: 'Remote recovery for files encrypted by BTC ransomware virus', 
+ info: 'Remote recovery for files encrypted by BTC ransomware virus' 
+                    },
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333304469958717442', 
+ title: 'EMC VNX LUN recovery', 
+ info: 'EMC VNX5700, 2 LUNs were offline due to power crash during replacing SPS,They were Thin Provision LUNs and could not be recovered' 
+                    },
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333304298281738240', 
+ title: 'AIX LVM JFS or JFS2 recovery', 
+ info: 'Using the AIX LVM JFS JFS2 recovery tool, the VGDA info and all the Logical partitions info were recognized and imported in AIX system' 
+                    },
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333304077325746176', 
+ title: 'V5000 RAID DATA RECOVERY', 
+ info: 'Two nodes of IBM V5000 failed and can not do T3 recovery'
+                    },
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333303114720428034', 
+ title: 'Oracle data recovery', 
+ info: 'Background:  The disk group of Database dg_cdr had 19x700G LUNs as asm  disks. one has a problem, add a new LUN and delete a wrong LUN, but  rebalance could not complete...'  
+                    },
+                    {
+ href: 'https://twitter.com/virmwareservice/status/1333301725592129536', 
+ title: 'DS6800 Both controller and both BBU replaced, all logic config lost', 
+ info: 'Background:DS6800 Both controller and both BBU replaced, all logic config lost' 
+                    },
+                ]
+            }
+        })
